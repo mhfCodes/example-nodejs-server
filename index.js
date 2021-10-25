5d2261e621829fa6e2a4fdf3c0f6492213e07168
mhf <mohammadhossein.farrokhi.78@gmail.com>
2021-10-26 02:47:24 +0330

    added package.json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 server.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World');
+    res.end(String(req));
 }).listen(PORT, () => console.log('Server Running'));
 
 
