e6aeb5928f7ca24293501d50d834faf63d9f9a04
mhf <mohammadhossein.farrokhi.78@gmail.com>
2021-10-26 04:06:42 +0330

    added other routes

--- index.js
+++ index.js
@@ -6,15 +6,46 @@ const PORT = process.env.PORT || 5000;
 
 server.createServer((req, res) => {
 
-    console.log(req.url);
+    const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    const extName = path.extname(filePath);
+    let contentType = 'text/html';
 
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-        if (err) throw err;        
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(content);
+    switch(extName) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (error, content) => {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(content, 'utf-8');
+                })
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(content, 'utf-8');
+        }
     })
 
-}).listen(PORT, () => console.log('Server Running'));
+}).listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
 
 
 
